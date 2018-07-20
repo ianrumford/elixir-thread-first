f87bd4b23913bc440a7375161c3985b290f18c12
孙艺盟 <sunyimeng@giabbs.com>
2018-07-20 15:09:51 +0800

    piping into a unary operator is deprecated, use the qualified name instead. format code indent.

---
+++ .formatter.exs
@@ -0,0 +1,7 @@
+# Used by "mix format"
+[
+  inputs: [
+    "mix.exs",
+    "{lib,test}/**/*.{ex,exs}"
+  ]
+]
--- test/thread_first_test.exs
+++ test/thread_first_test.exs
@@ -1,40 +1,44 @@
 defmodule ThreadFirstTests do
-use ExUnit.Case
-require ThreadFirstMacros
-import ThreadFirstMacros
-test "thread1: HelloWorld" do
+  use ExUnit.Case
+  require ThreadFirstMacros
+  import ThreadFirstMacros
 
-  result = thread_first do
-    "hello world" 
-    String.split 
-    Stream.map(&String.capitalize/1) 
-    Enum.join
+  test "thread1: HelloWorld" do
+    result =
+      thread_first do
+        "hello world"
+        String.split()
+        Stream.map(&String.capitalize/1)
+        Enum.join()
+      end
+
+    assert result == "HelloWorld"
   end
 
-  assert result == "HelloWorld"
+  test "thread2: single-line do block" do
+    result =
+      thread_first do
+        "hello world"
+      end
 
-end
-test "thread2: single-line do block" do
-
-  result = thread_first do
-    "hello world" 
+    assert result == "hello world"
   end
 
-  assert result == "hello world" 
+  test "thread3: a calculator" do
+    result =
+      thread_first do
+        # piping into a unary operator is deprecated, please use the qualified name.
+        # For example, Kernel.+(5), instead of +5
 
-end
-test "thread3: a calculator" do
+        42
+        # 47
+        Kernel.+(5)
+        # 30
+        Kernel.-(17)
+        # remainder is 4
+        rem(26)
+      end
 
-  result = thread_first do
-
-    42
-    + 5 # 47
-    - 17 # 30
-    rem 26 # remainder is 4 
-    
+    assert 4 == result
   end
-  
-  assert 4 == result 
-  
-end
 end
